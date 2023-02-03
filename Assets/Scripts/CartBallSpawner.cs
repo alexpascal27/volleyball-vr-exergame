56aaef8b064c43bcd342125a9b2165cba9e5695a
Alex Zamurca <alexzamurca@yahoo.co.uk>
2023-02-03 16:54:15 +0000

    Add scripts and scene

---
+++ Assets/Scripts/CartBallSpawner.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class CartBallSpawner : MonoBehaviour
+{
+    [SerializeField] private float upwardsOffsetSpawnPoint = 0.5f;
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private float timeInBetweenCheck = 2.0f;
+    [SerializeField] private float raycastLength =  2.0f;
+    
+    private RaycastHit hit;
+    private float currTimeCounter;
+    private Vector3 spawnPoint;
+
+    private void Start()
+    {
+        currTimeCounter = timeInBetweenCheck;
+        // above transform position
+        spawnPoint = transform.position + upwardsOffsetSpawnPoint * Vector3.up;
+    }
+
+    private void FixedUpdate()
+    {
+        if (currTimeCounter <= 0.0f)
+        {
+            // fire ray
+            bool hitSomething = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit,
+                raycastLength);
+            // if not hit, spawn
+            if (!hitSomething)
+            {
+                Instantiate(objectToSpawn, spawnPoint, transform.rotation);
+            }
+
+            // reset Timer
+            currTimeCounter = timeInBetweenCheck;
+        }
+        else currTimeCounter -= Time.deltaTime;
+    }
+}
