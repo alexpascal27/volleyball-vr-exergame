ed9f07691f7c2f5b16f720e35f4661ae494aa434
Alex Zamurca <alexzamurca@yahoo.co.uk>
2023-04-08 23:16:45 +0100

    Add option to limit movement in Z and X axis, as well as limit the range where movement is allowed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float playerSpeed = 3f;
     public SteamVR_Input_Sources handType;
+    public Vector3 limitFromBase;
+    public bool constraintX = false;
+    public bool constraintZ = false;
     private SteamVR_Action_Vector2 moveAction;
     
     // Start is called before the first frame update
     void Start()
     {
         moveAction = SteamVR_Actions.default_Move;
+        limitFromBase = new Vector3(Mathf.Abs(limitFromBase.x), Mathf.Abs(limitFromBase.y), Mathf.Abs(limitFromBase.z));
     }
 
     // Update is called once per frame
@@ -21,9 +25,15 @@ public class PlayerController : MonoBehaviour
         // x = horizontal, y = vertical
         Vector2 moveValue = moveAction.GetAxis(handType);
         
-        Vector3 direction = new Vector3(moveValue.x, 0, moveValue.y).normalized;
+        Vector3 direction = new Vector3(constraintX ? 0 : moveValue.x, 0, constraintZ ? 0 : moveValue.y).normalized;
         Vector3 movement = transform.TransformDirection(direction) * (playerSpeed * Time.deltaTime);
 
-        transform.position += movement;
+        if(!OutOfRange(transform.position + movement)) transform.position += movement;
+    }
+
+    bool OutOfRange(Vector3 pos)
+    {
+        return pos.x < -limitFromBase.x || pos.x > limitFromBase.x || 
+               pos.z < -limitFromBase.z || pos.z > 0;
     }
 }
