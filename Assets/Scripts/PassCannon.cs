92514c6bf5d2171e946f9b9cdc9ef63e17885ae0
Alex Zamurca <alexzamurca@yahoo.co.uk>
2023-04-08 19:06:44 +0100

    Add hitter spike animation and sync to look like it hits the receive

--- Assets/Scripts/PassCannon.cs
+++ Assets/Scripts/PassCannon.cs
@@ -9,14 +9,20 @@ public class PassCannon : MonoBehaviour
     [SerializeField] public GameObject ballPrefab;
     [SerializeField] public Vector3 defaultRotation = new Vector3(0f, 0f, 0f);
     [SerializeField] public Transform shotPoint;
-    [SerializeField] public Vector3 passTargetPoint;
+    // Outside position = Vector3(3.8, 3.2, 0.5)
+    // Hitter highest hit while hitting ^ = Vector3(4, 1.5, 1.08)
+    [SerializeField] public Vector3 basePassTargetPoint = new Vector3(0, 3.2f, 0.5f);
+    [SerializeField] public Vector3 leewayFromBaseTargetPoint = new Vector3(3.8f, 0, 0);
     [SerializeField] public float timeToGetToTarget;
 
     public GameObject targetPrefab;
+    public GameObject hitterPrefab;
+    // a bit to the left, much lower and behind
+    public Vector3 hitterPositionOffset = new Vector3(0.4f, -1.6f, 0.5f);
 
     private BoxCollider boxCollider;
     private int counter = 0;
-    
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider>();
@@ -45,36 +51,21 @@ public class PassCannon : MonoBehaviour
         // Spawn ball
         GameObject instantiatedBall = Instantiate(ballPrefab, shotPoint.position, Quaternion.Euler(Vector3.zero));
         Rigidbody rb = instantiatedBall.GetComponent<Rigidbody>();
-        Vector3 predictedVelocity = PredictVelocityGivenFinalPosition(rb, passTargetPoint, timeToGetToTarget);
+        Vector3 targetPoint = GenerateTargetPointAlongNet();
+        Vector3 predictedVelocity = PredictVelocityGivenFinalPosition(rb, targetPoint, timeToGetToTarget);
         rb.velocity = predictedVelocity;
         
-        //Vector3 predictedPosition = PredictPositionGivenY(rb, 0f);
-        //Instantiate(targetPrefab, predictedPosition, targetPrefab.transform.rotation);
+        Instantiate(targetPrefab, targetPoint, targetPrefab.transform.rotation);
+        Instantiate(hitterPrefab, targetPoint + hitterPositionOffset, hitterPrefab.transform.rotation);
     }
 
-    // Assumes no obstacles in the way
-    Vector3 PredictPositionGivenY(Rigidbody rb, float yCoordinate)
+    Vector3 GenerateTargetPointAlongNet()
     {
-        Vector3 r0 = rb.position;
-        Vector3 v0 = rb.velocity;
-        Vector3 a = new Vector3(0, -9.8f, 0);
-        float t0 = (-v0.y + Mathf.Sqrt(Mathf.Pow(v0.y, 2) - 2 * a.y * (r0.y - yCoordinate))) / a.y;
-        float t1 = (-v0.y - Mathf.Sqrt(Mathf.Pow(v0.y, 2) - 2 * a.y * (r0.y - yCoordinate))) / a.y;
-        
-        // need t1
-        if(t0 < 0 && t1 > 0)
-        {
-            return r0 + v0 * t1 + a * Mathf.Pow(t1, 2) / 2;
-        }
-        // need t0
-        if(t0 > 0 && t1 < 0)
-        {
-            return r0 + v0 * t0 + a * Mathf.Pow(t1, 2) / 2;
-        }
-        
-        // something wrong
-        Debug.LogError("BOTH T0 and T1 below 0:     t0: " + t0 + "    And  t1: " + t1);
-        return r0;
+        Vector3 offset = new Vector3(
+            UnityEngine.Random.Range(-leewayFromBaseTargetPoint.x, leewayFromBaseTargetPoint.x),
+            UnityEngine.Random.Range(-leewayFromBaseTargetPoint.y, leewayFromBaseTargetPoint.y),
+            UnityEngine.Random.Range(-leewayFromBaseTargetPoint.z, leewayFromBaseTargetPoint.z));
+        return basePassTargetPoint + offset;
     }
 
     Vector3 PredictVelocityGivenFinalPosition(Rigidbody rb, Vector3 r, float t)
