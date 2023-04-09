7d23b11ff56dcaa4ca5dbdc7cb3ffff83ac7c020
Alex Zamurca <alexzamurca@yahoo.co.uk>
2023-04-09 21:26:01 +0100

    Add all jump features (drop a bit tough but fine for now)

--- Assets/Scripts/PlayerJumpController.cs
+++ Assets/Scripts/PlayerJumpController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
@@ -8,13 +7,56 @@ public class PlayerJumpController : MonoBehaviour
     public SteamVR_Input_Sources oneHandType;
     public SteamVR_Input_Sources otherHandType;
     public SteamVR_Action_Boolean triggerHoldAction;
+    public Rigidbody playerRb;
+    public float groundHeight = 0f;
+    public float maxHeight = 2f;
+    public float jumpRate = 1f;
+    
+    private bool holdingBothTriggers = false;
+    private bool holdTriggerLoop = false;
+    private bool jumping = false;
+    private Coroutine jumpCoroutine;
 
-    // Update is called once per frame
     void Update()
     {
-        if (triggerHoldAction.GetState(oneHandType) && triggerHoldAction.GetState(otherHandType))
+        // Updating holdingBothTriggers boolean
+        if (triggerHoldAction.GetState(oneHandType) && triggerHoldAction.GetState(otherHandType)) holdingBothTriggers = true;
+        else holdingBothTriggers = false;
+
+        // to avoid loop where you hold down triggers, so you infinitely jump, reset and jump
+        if(!holdingBothTriggers && holdTriggerLoop) holdTriggerLoop = false;
+        
+        float currPlayerHeight = playerRb.position.y;
+        // Only start jumping if on ground, not jumping and holding both triggers
+        if (holdingBothTriggers && !jumping && currPlayerHeight <= groundHeight && !holdTriggerLoop)
         {
-            Debug.Log("Holding both hand triggers");
+            jumpCoroutine = StartCoroutine(JumpCoroutine());
+            Debug.Log("Holding both triggers, on ground and not jumping");
+        }
+        // If reached max height or leg go of triggers
+        if (currPlayerHeight >= maxHeight || (currPlayerHeight > groundHeight && !holdingBothTriggers && jumping))
+        {
+            StopCoroutine(jumpCoroutine);
+            jumping = false;
+            Debug.Log("Already in air, now droppping");
+            transform.position = new Vector3(transform.position.x, groundHeight, transform.position.z);
+            if (holdingBothTriggers) holdTriggerLoop = true;
+        }
+    }
+    
+    IEnumerator JumpCoroutine()
+    {
+        jumping = true;
+        
+        float startHeight = groundHeight;
+        float targetHeight = maxHeight;
+        float currPlayerHeight = playerRb.position.y;
+
+        while (currPlayerHeight < targetHeight)
+        {
+            currPlayerHeight += Time.deltaTime * jumpRate;
+            transform.position = new Vector3(transform.position.x, currPlayerHeight, transform.position.z);
+            yield return null;
         }
     }
 }
