d975c9dab9dd0db23efecea1d4b6b0155f088a9b
Alex Zamurca <alexzamurca@yahoo.co.uk>
2023-04-13 20:37:24 +0100

    Add script to only make ball bouncy when hand touches it

---
+++ Assets/Scripts/BallMaterialController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class BallMaterialController : MonoBehaviour
+{
+    public PhysicMaterial materialAfterCollision;
+    
+    private bool touchedPlayer = false;
+    private SphereCollider sphereCollider;
+    
+    private void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionPrefab = collision.gameObject;
+        if (collisionPrefab.CompareTag("Hand"))
+        {
+            if (!touchedPlayer)
+            {
+                Debug.Log("Touched hand for first time");
+                touchedPlayer = true;
+                sphereCollider.material = materialAfterCollision;
+            }
+        }
+    }
+}
